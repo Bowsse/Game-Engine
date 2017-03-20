51ea4a42510cc20588f8b6b0cb84b9c8fbd17099
Jaakko Pöyhönen <K1803@LABRANET.JAMK.FI>
2017-03-20 18:17:28 +0200

    Almost a window

--- GameEngine/Main.cpp
+++ GameEngine/Main.cpp
@@ -3,12 +3,18 @@ main.cpp
 entry point for a superduper game engine
 */
 
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+#include "IApplication.h"
 
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR pCmdLine, int nCmdShow)
 {
-	::MessageBox(NULL, L"Hello Game Engine", L"HELLO", MB_OK);
+	IApplication* pApp = new IApplication;
+	if (!pApp->Create())
+	{
+		::MessageBox(NULL, L"APP START FAILED", L"ERROR", MB_OK);
+		return 0;
+	}
+	pApp->Run();
+	delete pApp;
 	return 0;
 }
 	
